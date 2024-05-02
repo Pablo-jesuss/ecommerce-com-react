55499c7473854cf429135a6765b08f60dd387c12
Pablo-jesuss <pablo.programador26@gmail.com>
2024-05-02 17:55:27 -0300

    favicon

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Hello, React Developer!</h1>
+      <h1 className="text-primary m-5 pt-5">Hello, React Developer!</h1>
     </>
   );
 }
