5a07a32a2885a0035126c0289eb2800ec7be9cd1
Pablo-jesuss <pablo.programador26@gmail.com>
2024-05-03 18:54:50 -0300

    setHeader

--- ecommerce/src/components/NavItems.jsx
+++ ecommerce/src/components/NavItems.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NavItems = () => {
+  const [menuToggle, setMenuToggle] = useState(false);
+  const [socialToggle, setSocialToggle] = useState(false);
+  const [headerFixed, setHeaderFixed] = useState(false);
+
+  // addevent listener
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      setHeaderFixed(true);
+    }
+  });
   return <div>NavItems</div>;
 };
 
