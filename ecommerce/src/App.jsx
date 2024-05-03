5d22412a1b6b9364431de164b80911cba9e0a9d2
Pablo-jesuss <pablo.programador26@gmail.com>
2024-05-03 18:47:40 -0300

    navItems

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import NavItems from "./components/NavItems";
 
 function App() {
   return (
     <>
-      <nav>Nav bar</nav>
+      <NavItems />
       <Outlet />
     </>
   );
