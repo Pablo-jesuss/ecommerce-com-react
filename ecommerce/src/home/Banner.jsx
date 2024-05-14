31a987eb4d1bf635c14a65d67f6cd85fdd1a85ed
Pablo-jesuss <pablo.programador26@gmail.com>
2024-05-14 17:11:32 -0300

    banner

---
+++ ecommerce/src/home/Banner.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const title = <h2>Search Your One From Thousand of Products</h2>;
+
+const desc = "We have the largest collection of products";
+
+const bannerList = [
+  {
+    iconName: "icofont-users-alt-4",
+    text: "1.5 Million Customers",
+  },
+  {
+    iconName: "icofont-notification",
+    text: "More then 2000 Marchent",
+  },
+  {
+    iconName: "icofont-globe",
+    text: "Buy Anything Online",
+  },
+];
+
+const Banner = () => {
+  return <div>Banner</div>;
+};
+
+export default Banner;
