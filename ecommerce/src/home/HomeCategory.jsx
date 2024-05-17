a818525af12657bcb84df52be01b836f7b623cf8
Pablo-jesuss <pablo.programador26@gmail.com>
2024-05-17 10:08:12 -0300

    category-content

--- ecommerce/src/home/HomeCategory.jsx
+++ ecommerce/src/home/HomeCategory.jsx
@@ -56,13 +56,23 @@ const HomeCategory = () => {
 
         {/* section card */}
         <div className="section-wrapper">
-          <div>
+          <div className="row g-4 justify-content-center row-cols-md-3 row-cols-sm-2 row-cols-1">
             {categoryList.map((val, i) => (
               <div key={i} className="col">
                 <Link to="/shop" className="category-item">
                   <div className="category-inner">
+                    {/* image thumbnail */}
                     <div className="category-thumb">
                       <img src={val.imgUrl} alt="" />
+                    </div>
+                    {/* content */}
+                    <div className="category-content">
+                      <div className="cate-icon">
+                        <i className={val.iconName}></i>
+                      </div>
+                      <Link>
+                        <h6>{val.title}</h6>
+                      </Link>
                     </div>
                   </div>
                 </Link>
