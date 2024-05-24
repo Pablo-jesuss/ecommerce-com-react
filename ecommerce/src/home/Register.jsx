8b186464dd27cc030fbb877791c8790d3416e04c
Pablo-jesuss <pablo.programador26@gmail.com>
2024-05-24 13:22:18 -0300

    formRegister

--- ecommerce/src/home/Register.jsx
+++ ecommerce/src/home/Register.jsx
@@ -20,10 +20,42 @@ const Register = () => {
         <div className="row g-4 row-cols-lg-2 row-cols-1 align-items-center">
           <div className="col">
             <div className="section-header">
-              <span>{subTitle}</span>
+              <span className="subTitle">{subTitle}</span>
+              {title}
+              <p>{desc}</p>
             </div>
           </div>
-          <div className="col"></div>
+          <div className="col">
+            <div className="section-wrapper">
+              <h4>Register Now</h4>
+              <form className="register-form">
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Username"
+                  className="reg-input"
+                />
+
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  className="reg-input"
+                />
+
+                <input
+                  type="number"
+                  name="number"
+                  placeholder="Phone"
+                  className="reg-input"
+                />
+
+                <button type="submit" className="lab-btn">
+                  Register Now
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </section>
