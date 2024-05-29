7f79ebcd96c8a1479d34412b8474f812983ee418
Pablo-jesuss <pablo.programador26@gmail.com>
2024-05-29 14:38:38 -0300

    searchShow

--- ecommerce/src/shop/Search.jsx
+++ ecommerce/src/shop/Search.jsx
@@ -1,8 +1,51 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Search = ({ products, GridList }) => {
-  const [seachTerm, setSearchTerm] = useState("");
-  return <div></div>;
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  return (
+    <div className="widget widget-search">
+      <form className="search-wrapper mb-3">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search..."
+          defaultValue={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit">
+          <i className="icofont-search-2"></i>
+        </button>
+      </form>
+
+      {/* showing search */}
+      <div>
+        {searchTerm &&
+          filteredProducts.map((product) => (
+            <Link key={product.id} to={`/shop/${product.id}`}>
+              <div className="d-flex gap-3 p-2">
+                <div className="pro-thumb h-25">
+                  <img
+                    src={product.img}
+                    alt=""
+                    className="flex-{grow|shrink}-0"
+                  />
+                </div>
+                <div className="product-content">
+                  <p>
+                    <Link to={`/shop/${product.id}`}>{product.name}</Link>
+                  </p>
+                </div>
+              </div>
+            </Link>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
