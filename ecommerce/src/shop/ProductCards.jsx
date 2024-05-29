f2269ab05f9b2178d15592dea4cf8898539b02c0
Pablo-jesuss <pablo.programador26@gmail.com>
2024-05-29 10:59:20 -0300

    productCards

--- ecommerce/src/shop/ProductCards.jsx
+++ ecommerce/src/shop/ProductCards.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Ratting from "../components/Ratting";
 
 const ProductCards = ({ GridList, products }) => {
   return (
@@ -15,6 +17,29 @@ const ProductCards = ({ GridList, products }) => {
               <div className="pro-thumb">
                 <img src={product.img} alt="" />
               </div>
+
+              {/* product action links */}
+              <div className="product-action-link">
+                <Link to={`/shop/${product.id}`}>
+                  <i className="icofont-eye"></i>
+                </Link>
+                <a href="#">
+                  <i className="icofont-heart"></i>
+                </a>
+                <Link to="/cart-page">
+                  <i className="icofont-cart-alt"></i>
+                </Link>
+              </div>
+            </div>
+
+            {/* product content */}
+            <div className="product-content">
+              <h5>
+                <Link to={`/shop/${product.id}`}>{product.name}</Link>
+                <p className="productRating">
+                  <Ratting />
+                </p>
+              </h5>
             </div>
           </div>
         </div>
