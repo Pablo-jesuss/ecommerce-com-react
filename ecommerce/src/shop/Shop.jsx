941d531fb203fa9ebeed51a83bfc083ad7e72a39
Pablo-jesuss <pablo.programador26@gmail.com>
2024-05-28 22:59:24 -0300

    right side

--- ecommerce/src/shop/Shop.jsx
+++ ecommerce/src/shop/Shop.jsx
@@ -5,6 +5,15 @@ const Shop = () => {
   return (
     <div>
       <PageHeader title="Our Shop Page" curPage="Shop" />
+
+      <div className="shop-page padding-tb">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-12">left side</div>
+            <div className="col-lg-4 col-12">right side</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
