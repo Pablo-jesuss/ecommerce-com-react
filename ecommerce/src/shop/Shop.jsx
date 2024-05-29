e50afda9875fe68c0bc053771674a7171a6e6b80
Pablo-jesuss <pablo.programador26@gmail.com>
2024-05-29 10:14:44 -0300

    gridList

--- ecommerce/src/shop/Shop.jsx
+++ ecommerce/src/shop/Shop.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import PageHeader from "../components/PageHeader";
 
+const ShowResults = "Showing 01 - 12 of 139 Results";
+
 const Shop = () => {
+  const [GridList, setGridList] = useState(true);
   return (
     <div>
       <PageHeader title="Our Shop Page" curPage="Shop" />
@@ -9,7 +12,22 @@ const Shop = () => {
       <div className="shop-page padding-tb">
         <div className="container">
           <div className="row justify-content-center">
-            <div className="col-lg-8 col-12">left side</div>
+            <div className="col-lg-8 col-12">
+              <article>
+                <div className="shop-title d-flex flex-warp justify-content-between">
+                  <p>{ShowResults}</p>
+                  <div
+                    className={`product-view-mode ${
+                      GridList ? "gridActive" : "listActive"
+                    }`}
+                  >
+                    <a className="grid" onClick={() => setGridList(!GridList)}>
+                      <i className="icofont-ghost"></i>
+                    </a>
+                  </div>
+                </div>
+              </article>
+            </div>
             <div className="col-lg-4 col-12">right side</div>
           </div>
         </div>
