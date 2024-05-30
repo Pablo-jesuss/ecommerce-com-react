9c82a22bb8edb76c46b3022dafd51b7ac13f24e0
Pablo-jesuss <pablo.programador26@gmail.com>
2024-05-30 11:05:05 -0300

    pageHeader

--- ecommerce/src/shop/SingleProduct.jsx
+++ ecommerce/src/shop/SingleProduct.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import PageHeader from "../components/PageHeader";
 
 const SingleProduct = () => {
-  return <div></div>;
+  const [product, setProduct] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch("/src/products.json")
+      .then((res) => res.json())
+      .then((data) => setProduct(data));
+  }, []);
+
+  const result = product.filter((p) => p.id === id);
+  console.log(result);
+  return (
+    <div>
+      <PageHeader title={"OUR SHOP SINGLE"} curPage={"Shop / Single Product"} />
+    </div>
+  );
 };
 
 export default SingleProduct;
