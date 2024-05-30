d7b2d2eef3e93af7e8dc9136e829a4ca570be031
Pablo-jesuss <pablo.programador26@gmail.com>
2024-05-30 11:40:04 -0300

    SinglePro

--- ecommerce/src/shop/SingleProduct.jsx
+++ ecommerce/src/shop/SingleProduct.jsx
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import PageHeader from "../components/PageHeader";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
+
 const SingleProduct = () => {
   const [product, setProduct] = useState([]);
   const { id } = useParams();
@@ -17,6 +23,71 @@ const SingleProduct = () => {
   return (
     <div>
       <PageHeader title={"OUR SHOP SINGLE"} curPage={"Shop / Single Product"} />
+
+      <div className="shop-single padding-tb aside-bg">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-12">
+              <article>
+                <div className="product-details">
+                  <div className="row align-items-center">
+                    <div className="col-md-6 col-12">
+                      <div className="product-thumb">
+                        <div className="swiper-container pro-single-top">
+                          <Swiper
+                            spaceBetween={30}
+                            slidesPerView={1}
+                            loop={"true"}
+                            autoplay={{
+                              delay: 2000,
+                              disableOnInteraction: false,
+                            }}
+                            modules={{ Autoplay }}
+                            navigation={{
+                              prevEl: ".pro-single-prev",
+                              nextEl: ".pro-single-next",
+                            }}
+                            className="mySwiper"
+                          >
+                            {result.map((item, i) => (
+                              <SwiperSlide key={i}>
+                                <div className="single-thumb">
+                                  <img src={item.img} alt="" />
+                                </div>
+                              </SwiperSlide>
+                            ))}
+                          </Swiper>
+                          <div className="pro-single-next">
+                            <i className="icofont-rounded-left"></i>
+                          </div>
+                          <div className="pro-single-prev">
+                            <i className="icofont-rounded-right"></i>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-6 col-12">
+                      <div className="post-content">
+                        <div>
+                          {result.map((item) => (
+                            <p key={item.id}>{item.name}</p>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                {/* reviews */}
+                <div className="review">review</div>
+              </article>
+            </div>
+
+            {/* right side */}
+            <div className="col-lg-4 col-12">Right Side</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
