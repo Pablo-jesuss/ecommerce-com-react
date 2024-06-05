2fdf7e51969917238d6a90750f03e916962b5583
Pablo-jesuss <pablo.programador26@gmail.com>
2024-06-05 11:06:38 -0300

    updateLocal

--- ecommerce/src/shop/CartPage.jsx
+++ ecommerce/src/shop/CartPage.jsx
@@ -18,6 +18,34 @@ const CartPage = () => {
   const handleIncrease = (item) => {
     item.quantity += 1;
     setcartItems([...cartItems]);
+
+    // update local storage with new cart items
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  };
+
+  // handle quantity decrease
+  const handleDecrease = (item) => {
+    if (item.quantity > 1) {
+      item.quantity -= 1;
+      setcartItems([...cartItems]);
+
+      // update local storage with new cart items
+      localStorage.setItem("cart", JSON.stringify(cartItems));
+    }
+  };
+
+  // handle item remove
+  const handleRemoveItem = (item) => {
+    const updatedCart = cartItems.filter((cartItem) => cartItem.id !== item.id);
+
+    // update new cart
+    setcartItems(updatedCart);
+
+    updateLocalStorage(updatedCart);
+  };
+
+  const updateLocalStorage = (cart) => {
+    localStorage.setItem("cart", JSON.stringify(cart));
   };
   return <div>CartPage</div>;
 };
