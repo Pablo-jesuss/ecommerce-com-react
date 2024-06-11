997f44bac097268cfa2dd1d40c2877aadf74872f
Pablo-jesuss <pablo.programador26@gmail.com>
2024-06-11 10:08:58 -0300

    modalDialog

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import NavItems from "./components/NavItems";
 import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- ecommerce/src/shop/CheckOutPage.jsx
+++ ecommerce/src/shop/CheckOutPage.jsx
@@ -1,17 +1,37 @@
 import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const CheckOutPage = () => {
   const [show, setShow] = useState(false);
   const [activeTab, setActiveTab] = useState("visa");
 
-  // handle Tab change
   const handleTabChange = (tabId) => {
     setActiveTab(tabId);
   };
 
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
-  return <div className="modalCart"></div>;
+
+  return (
+    <div className="modalCart">
+      <Button variant="primary" className="py-2" onClick={handleShow}>
+        Fazer o check-out
+      </Button>
+
+      <Modal
+        className="modal fade"
+        show={show}
+        onHide={handleClose}
+        animation={false}
+        centered
+      >
+        <div className="modal-dialog">
+          <h5 className="px-3 mb-3">Selecione seu metodo de pagamento</h5>
+        </div>
+      </Modal>
+    </div>
+  );
 };
 
 export default CheckOutPage;
