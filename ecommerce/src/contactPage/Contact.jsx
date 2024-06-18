882d46bbe9df1c94db0d460e0a3054c1724951ea
Pablo-jesuss <pablo.programador26@gmail.com>
2024-06-18 11:54:47 -0300

    mapAdress

--- ecommerce/src/contactPage/Contact.jsx
+++ ecommerce/src/contactPage/Contact.jsx
@@ -5,6 +5,11 @@ const Contact = () => {
   return (
     <div>
       <PageHeader title={"Entre em contato conosco"} curPage={"Contate-nos"} />
+      <div className="map-adress-section padding-tb section-bg">
+        <div className="container">
+          <div className="row justify-content-center row-cols-xl-2 row-cols-1"></div>
+        </div>
+      </div>
     </div>
   );
 };
