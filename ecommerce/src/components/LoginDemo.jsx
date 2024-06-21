b643bc6c8a069085a1d2cde3a10803e72e536001
Pablo-jesuss <pablo.programador26@gmail.com>
2024-06-21 13:06:19 -0300

    login

--- ecommerce/src/components/LoginDemo.jsx
+++ ecommerce/src/components/LoginDemo.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const LoginDemo = () => {
-  return <div></div>;
+  return (
+    <div className="m-5 p-5">
+      <button className="bg-primary px-4 text-white">Login</button>
+    </div>
+  );
 };
 
 export default LoginDemo;
