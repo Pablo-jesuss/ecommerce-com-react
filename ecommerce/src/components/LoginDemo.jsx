d1e04041b00cc19c914cf407e3b8c5ed829602f4
Pablo-jesuss <pablo.programador26@gmail.com>
2024-06-21 20:40:21 -0300

    handleLogin

--- ecommerce/src/components/LoginDemo.jsx
+++ ecommerce/src/components/LoginDemo.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 const LoginDemo = () => {
+  const handleLogin = () => {
+    console.log();
+  };
   return (
     <div className="m-5 p-5">
-      <button className="bg-primary px-4 text-white">Login</button>
+      <button className="bg-primary px-4 text-white" onClick={handleLogin}>
+        Login
+      </button>
     </div>
   );
 };
